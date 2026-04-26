cd39a7230f914ff62f18dfcac18240836168f51b
Sebastian Quispe <sebastian.quispe.b@utec.edu.pe>
2026-04-25 22:58:16 -0500

    fix: opt into react router v7 flags

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,12 @@ const PlaceholderPage = ({ title }) => (
 
 function App() {
   return (
-    <Router>
+    <Router
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <Routes>
         <Route path="/" element={<Navigate to="/analytics" replace />} />
         
