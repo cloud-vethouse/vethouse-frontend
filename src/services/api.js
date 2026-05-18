57a3db0f5269d6853f7420877d1177db0cd79fea
CarlosVillegas <villegasarcecarlos@gmail.com>
2026-05-17 23:28:26 -0500

    Fix all2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://alb-vethouse-1686491810.us-east-1.elb.amazonaws.com',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
